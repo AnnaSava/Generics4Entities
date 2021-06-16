02e531fc4d1316e46a2a2c23e8271ef7add7f0c6
Anna Savina <anna.s.savina@gmail.com>
2021-06-16 20:12:46 +0300

    More base classes

--- Generics4Entities/BaseEntites.cs
+++ Generics4Entities/BaseEntites.cs
@@ -1,25 +1,48 @@
-﻿using System;
+﻿using Generics4Entities.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Generics4Entities
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IEntity<int>
     {
         public int Id { get; set; }
     }
 
-    public abstract class BaseRestorableEntity : BaseEntity
+    public abstract class BaseUpdatableEntity : BaseEntity, IUpdatable
+    {
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public abstract class BaseRestorableEntity : BaseUpdatableEntity, IRestorable
     {
         public bool IsDeleted { get; set; }
     }
 
-    public abstract class BaseNamedEntity : BaseEntity
+    public abstract class BaseNamedEntity : BaseEntity, INamed
     {
         public string Name { get; set; }
     }
 
-    public abstract class BaseNamedRestorableEntity : BaseRestorableEntity
+    public abstract class BaseUpdatableNamedEntity : BaseNamedEntity, IUpdatable, INamed
+    {
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public abstract class BaseNamedUpdatableEntity : BaseUpdatableEntity, IUpdatable, INamed
+    {
+        public string Name { get; set; }
+    }
+
+    public abstract class BaseRestorableNamedEntity : BaseNamedEntity, IRestorable, INamed
+    {
+        public DateTime LastUpdated { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+
+    public abstract class BaseNamedRestorableEntity : BaseRestorableEntity, IRestorable, INamed
     {
         public string Name { get; set; }
     }
---
+++ Generics4Entities/Interfaces/EntityInterfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics4Entities.Interfaces
+{
+    public interface IEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    public interface IUpdatable
+    {
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public interface IRestorable : IUpdatable
+    {
+        public bool IsDeleted { get; set; }
+    }
+
+    public interface INamed
+    {
+        public string Name { get; set; }
+    }
+}
